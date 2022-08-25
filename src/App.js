ca2ab6ae897ecd1775722fef039140ee5363d40c
Jubair Hossain <jubair.hossain@yahoo.com>
2022-08-25 15:57:53 +0600

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import MainContainer from './components/MainContainer';
+
+function App() {
+  return (
+    <>
+    <Header/>
+   <MainContainer/>
+   </>
+  );
+}
+
+export default App;
