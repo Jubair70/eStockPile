cae7dda6e1479265905bb382cb6052a0dd0e4f99
Jubair Hossain <jubair.hossain@yahoo.com>
2022-08-26 20:14:07 +0600

    Cart Onclick

---
+++ src/redux/actions/cart/index.js
@@ -0,0 +1,12 @@
+import * as actionTypes from "./types";
+
+export const addCartItem = (data) => async(dispatch)=>{
+    dispatch(_addCartItem(data));
+}
+
+export const _addCartItem = (data) => {
+    return {
+        type:actionTypes.ADD_CART_ITEM,
+        data
+    }
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import { topMenu } from "./topMenu";
 import {product} from "./product";
-
+import * as cart from './cart';
 const rootReducer = combineReducers({
     topMenu,
-    product
+    product,
+    ...cart
 });
 
 export default rootReducer;
